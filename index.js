88a3f018d7a7d45e24013cdfbc721046f1262a55
jlimm <limjaeminn@gmail.com>
2021-06-16 17:51:20 +0900

    added dependencies

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+// middleware
+app.use(express.json());
+// routes
+// registration
+
+// login
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "li" && password === "123456") {
+    const access_token = jwt.sign({ sub: username }, );
+    return res.json({ status: true, message: "login success" });
+  }
+  return res.status(401).json({ status: true, message: "login failed" });
+});
+
+app.listen(8888, () => console.log("server is running.."));
